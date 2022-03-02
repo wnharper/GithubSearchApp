d99b0a4de14c89aaa0cfd672ff26a17419d64f08
Warren Harper <warren.harper@gmail.com>
2022-03-02 15:05:11 -0800

    fixed alt tag in spinner

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -3,7 +3,7 @@ import spinner from "./assets/spinner.webp"
 const Spinner = () => {
   return (
     <div className='container flex justify-center'>
-      <img src={spinner} />
+      <img src={spinner} alt='retro windows file gif' />
     </div>
   )
 }
